0c7ee5db81397ae5d4c07a8e1e5289d7339536e0
MrHofmann <mdfkprekid@gmail.com>
2018-08-29 02:40:31 +0200

    Smece

--- band_pass.cpp
+++
@@ -1,7 +0,0 @@
-#include "band_pass.h"
-
-/*Band_Pass::Band_Pass()
-    :Filter(300,15000)
-{
-}
-*/
--- high_pass.cpp
+++
@@ -1,7 +0,0 @@
-#include "high_pass.h"
-
-High_Pass::High_Pass(int len)
-    :Filter(0,12000, 0, len)
-{
-
-}
